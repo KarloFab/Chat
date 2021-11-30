4dde9860f9c0610591d3edc656ab5396e6371bd7
KarloFab <fabijanickarlo1@gmail.com>
2021-11-30 23:08:43 +0100

    Changed titleLabel to CLTypingLabel

--- Chat/Controller/ViewController.swift
+++ Chat/Controller/ViewController.swift
@@ -6,14 +6,16 @@
 //
 
 import UIKit
+import CLTypingLabel
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var titleLabel: CLTypingLabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        titleLabel.text = "CHAT"
     }
 
 
