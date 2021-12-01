88403313741b7879f6e8e45ea3af65cc1cbbd96f
KarloFab <fabijanickarlo1@gmail.com>
2021-12-01 23:04:24 +0100

    Added Firebase dependency

--- Chat/Controller/RegisterViewController.swift
+++ Chat/Controller/RegisterViewController.swift
@@ -6,12 +6,14 @@
 //
 
 import UIKit
+import Firebase
 
 class RegisterViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        FirebaseApp.configure()
     }
 
 }
