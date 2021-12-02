812b095864ea0af2490dc1aba734f57e2ba06b56
KarloFab <fabijanickarlo1@gmail.com>
2021-12-02 10:05:18 +0100

    Added register action

--- Chat/Controller/RegisterViewController.swift
+++ Chat/Controller/RegisterViewController.swift
@@ -9,12 +9,27 @@ import UIKit
 import Firebase
 
 class RegisterViewController: UIViewController {
-
+    
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         FirebaseApp.configure()
     }
-
+    
+    @IBAction func registerPressed(_ sender: Any) {
+        if let email = emailTextField.text, let password = passwordTextField.text {
+            Auth.auth().createUser(withEmail: email, password: password) { authResult, error in
+                if let err = error {
+                    print(err)
+                } else {
+                    self.performSegue(withIdentifier: "RegisterToChat", sender: self)
+                }
+            }
+        }
+    }
+    
 }
 
